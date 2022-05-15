e00615bab26785511e070805f7d7ba2576a12d63
Leon Heuer <leon.heuer@outlook.de>
2022-05-15 13:56:20 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `SkyCave`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "-NecKRBBOmrKgSlUvp-sx-YzQu9kT3DefDSIT_M8cZ4",
+      "spaceId": "x3f34baq742d"
+    }
+  }, "gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
+};
